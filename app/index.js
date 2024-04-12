404c0fefb6ead026443a4141bc93613bc8ebe435
Sai Sashreek D <saisashreekd@gmail.com>
2024-04-12 10:51:24 +0530

    New Layout Updated

--- app/index.js
+++ app/index.js
@@ -1,11 +1,26 @@
-import { View, Text } from "react-native";
+import { useState } from "react";
+import { View, ScrollView, SafeAreaView } from "react-native";
+import { Stack, useRouter } from "expo-router";
+
+import { COLORS, icons, images, SIZES } from "../constants";
+import {
+  Nearbyjobs,
+  Popularjobs,
+  ScreenHeaderBtn,
+  Welcome,
+} from "../components";
 
 const Home = () => {
-    return (
-        <View>
-            <Text>Home Screen....</Text>
-        </View>
-    );
+  const router = useRouter();
+  return (
+    <SafeAreaView style={{ flex: 1, backgroundColor: COLORS.lightWhite }}>
+      <Stack.Screen
+        options={{
+          headerStyle: { backgroundColor: COLORS.lightWhite },
+        }}
+      />
+    </SafeAreaView>
+  );
 };
 
 export default Home;
